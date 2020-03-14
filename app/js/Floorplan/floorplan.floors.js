cebead769b187a4805f73f44420bc6db8e847d3d
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2020-03-14 17:48:05 +0100

    Floors

--- app/js/Floorplan/floorplan.floors.js
+++ app/js/Floorplan/floorplan.floors.js
@@ -15,9 +15,8 @@ class Floorplan_Floors {
 
     retrieveAllFloors() {
         //retrieve the floors from database
-        var token = getToken();
         var url = "https://progparty-homey-floorplan.azurewebsites.net/api/GetAllFloors?code=sl30LU/RJhGWyaUj1zQKrXARPTGEyW/aZCuFRP6mR41GL7G7x0ihlA==";
-        url += "&token=" + token;
+        url += "&token=" + getToken();
 
         console.log(url);
         $.get(url, function(json) {
@@ -61,9 +60,8 @@ class Floorplan_Floors {
     }
 
     addFloor(name, order, image) {
-        var token = getToken();
         var url = "https://progparty-homey-floorplan.azurewebsites.net/api/AddFloor?code=DZOZHKHYawTcGf/o19ZVi5CAmeciz747E2lWLHmEqQa3PHf1RJHQiQ==";
-        url += "&token=" + token;
+        url += "&token=" + getToken();
         url += "&order=" + order;
         url += "&image=" + image;
         url += "&name=" + name;
@@ -76,14 +74,25 @@ class Floorplan_Floors {
     }
 
     updateFloor(floorId, name, order, image) {
-        var token = getToken();
         var url = "https://progparty-homey-floorplan.azurewebsites.net/api/UpdateFloor?code=O6ac1A8x8B1i5H55wszVK5WQhsOjXy8k8nsWIn/MpTk9jQyoMgZp/Q==";
-        url += "&token=" + token;
+        url += "&token=" + getToken();
         url += "&floorId=" + floorId;
         url += "&order=" + order;
         url += "&image=" + image;
         url += "&name=" + name;
 
+        console.log(url);
+        $.get(url, function(json) {
+            console.log(`Updating floor worked (${json})`);
+            _floors.retrieveAllFloors();
+        });
+    }
+
+    deleteFloor(floorId) {
+        var url = "https://progparty-homey-floorplan.azurewebsites.net/api/DeleteFloor?code=n8pqdURM7bxOQW9XyfTq88Kc2NxbQIEdxkzmAuhGljS3zFwiS8bmPQ==";
+        url += "&token=" + getToken();
+        url += "&floorId=" + floorId;
+        
         console.log(url);
         $.get(url, function(json) {
             console.log(`Removing floor worked (${json})`);
