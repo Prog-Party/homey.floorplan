f887a7fded6210d8c33d3dd9fb740da9e3da5683
Dennis Rosenbaum <dennis.rosenbaum@outlook.com>
2020-03-14 16:24:34 +0100

    Adding Floors to DB

--- app/js/Floorplan/floorplan.floors.js
+++ app/js/Floorplan/floorplan.floors.js
@@ -11,6 +11,9 @@ class Floorplan_Floors {
 
         this.initializeEvents();
 
+    }
+
+    retrieveAllFloors() {
         //retrieve the floors from database
         var token = getToken();
         var functionUrl = "https://progparty-homey-floorplan.azurewebsites.net/api/GetAllFloors?code=sl30LU/RJhGWyaUj1zQKrXARPTGEyW/aZCuFRP6mR41GL7G7x0ihlA==";
@@ -27,6 +30,9 @@ class Floorplan_Floors {
 
     initializeButtons() {
         this.allFloors.forEach(floor => {
+            _floors.floorLayoutMenu.html("");
+            _floors.floorEditLayoutMenu.html("");
+
             var button = `<a class='floor-button' data-floor-id='${floor.id}' href='#'>${floor.name}</a><br />`;
             _floors.floorLayoutMenu.append(button);
             _floors.floorEditLayoutMenu.append(button);
@@ -52,6 +58,21 @@ class Floorplan_Floors {
         var floor = _floors.allFloors.filter(f => f.id == floorId)[0];
         this._activeFloor = floor;
         this.floorCanvas.css("background-image", `url(${floor.img})`);
+    }
+
+    addFloor(name, order, image) {
+        var token = getToken();
+        var functionUrl = "https://progparty-homey-floorplan.azurewebsites.net/api/AddFloor?code=DZOZHKHYawTcGf/o19ZVi5CAmeciz747E2lWLHmEqQa3PHf1RJHQiQ=="
+        var url = functionUrl + "&token=" + token;
+        url += "&order=" + order;
+        url += "&image=" + image;
+        url += "&name=" + name;
+
+        console.log(url);
+        $.get(url, function(json) {
+            console.log(`Adding floor worked (${json})`);
+            _floors.retrieveAllFloors();
+        });
     }
 
     get activeFloor() {
