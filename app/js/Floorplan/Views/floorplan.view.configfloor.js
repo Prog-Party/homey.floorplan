95352af8f94e6b6a1b8a4d7267d1be2e5b7abbb4
Jens den Braber <jens.denbraber@gmail.com>
2020-03-15 09:29:54 +0100

    - hide buttons when not applicable

--- app/js/Floorplan/Views/floorplan.view.configfloor.js
+++ app/js/Floorplan/Views/floorplan.view.configfloor.js
@@ -14,14 +14,24 @@ document.addEventListener('onFloorsRetrieved', function (e) {
         var button = `<div><a class='floor-button' data-floor-id='${floor.id}' href='#'>${floor.name}</a><span> > ${floor.order}</span></div>`;
         floorSwitcherMenu.append(button);
     });
-        
-    if(_floors.allFloors.length > 0) {
+
+    if(_floors.allFloors.length == 0) {
+        $("#configfloor_UpdateFloor").hide();
+        $("#configfloor_DeleteFloor").hide();
+    } else {
         _floors.activateFloor(_floors.allFloors[0].id);
+        $("#configfloor_UpdateFloor").show();
+        $("#configfloor_DeleteFloor").show();
     }
 }, false);
 
 $(document).on("click", "#configfloor_AddFloor", function() {
     _floors.addFloor($("#configfloor_name").val(), $("#configfloor_order").val(), $("#configfloor_image").val());
+
+    if(_floors.allFloors.length > 0) {
+        $("#configfloor_UpdateFloor").show();
+        $("#configfloor_DeleteFloor").show();
+    }
 });
 
 $(document).on("click", "#configfloor_UpdateFloor", function() {
