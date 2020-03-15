1e7393df050c50482e27fa2fc37b5386cd2a8425
Jens den Braber <jens.denbraber@gmail.com>
2020-03-15 18:26:47 +0100

    - adjusted html for configdevice_buttons
    - added logic for update and delete device

--- app/js/Floorplan/Views/floorplan.view.configdevice.js
+++ app/js/Floorplan/Views/floorplan.view.configdevice.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', function() {
+    _devices.initialize();
+});
+
 document.addEventListener('onActivateFloor', function (e) { 
 
     $("#configdevice_Canvas").css("background-image", `url(${_floors.activeFloor.img})`);
@@ -22,3 +26,25 @@ document.addEventListener('onActivateDevice', function (e) {
     $("#configdevice_DeviceName").html(_devices.activeHomeyDevice.name);
 }, false);
 
+$(document).on("click", ".homey-device-to-floorplan-button", function() {
+    var homeydeviceId = $(this).attr("data-device-id");
+    _devices.activateHomeyDevice(homeydeviceId);
+    _devices.addDevice(homeydeviceId, 50, 50, _floors.activeFloor.id);
+});
+
+$(document).on("click", "#configfloor_AddFloor", function() {
+    _floors.addFloor($("#configfloor_name").val(), $("#configfloor_order").val(), $("#configfloor_image").val());
+
+    if(_floors.allFloors.length > 0) {
+        $("#configfloor_UpdateFloor").show();
+        $("#configfloor_DeleteFloor").show();
+    }
+});
+
+$(document).on("click", "#configdevice_UpdateDevice", function() {
+    _devices.updateDevice(_devices.activeHomeyDevice.id, $("#configdevice_DeviceLocationX").val(), $("#configdevice_DeviceLocationY").val(), _floors.activeFloor.id);
+});
+
+$(document).on("click", "#configdevice_DeleteDevice", function() {
+    _devices.deleteDevice(_devices.activeHomeyDevice.id);
+});
